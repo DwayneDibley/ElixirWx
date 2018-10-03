99d81c1de21e4867df1674f5c1cd4c711444d6e0
DwayneDibley <roger.wenham@gmx.net>
2018-10-03 20:27:42 +0200

    Add files via upload

---
+++ lib/elixirwx.ex
@@ -0,0 +1,18 @@
+defmodule Elixirwx do
+  @moduledoc """
+  Documentation for Elixirwx.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Elixirwx.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
